a10baa9054c827b19ca4ca13cfc3a2624a534c14
Joshua Marshall <jmarsh0614@gmail.com>
2017-02-28 23:08:48 -0500

    Add project files.

---
+++ ReadTextFileWhile/Module1.vb
@@ -0,0 +1,30 @@
+﻿
+
+Imports System.IO
+
+Module Module1
+
+
+
+    Sub Main()
+
+
+        Dim myReader As StreamReader = New StreamReader("Value.txt")
+        Dim line As String = ""
+
+        While Not IsNothing(line)
+            line = myReader.ReadLine()
+            If Not IsNothing(line) Then
+                Console.WriteLine(line)
+            End If
+        End While
+
+        myReader.Close()
+        Console.ReadLine()
+
+
+
+        Console.WriteLine()
+    End Sub
+
+End Module
